77a7bc7dbc70a4cc8d42d3e547119763218fe112
jianghailong <jianghailong@eptok.com>
2019-05-09 14:22:13 +0800

    修改结构

---
+++ dragon-root/dragon-springboot/src/main/java/com/dragon/springboot/web/DragonWebApplication.java
@@ -0,0 +1,14 @@
+package com.dragon.springboot.web;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DragonWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DragonWebApplication.class, args);
+	}
+
+}
---
+++ dragon-root/dragon-springboot/src/main/java/com/dragon/springboot/web/ServletInitializer.java
@@ -0,0 +1,14 @@
+package com.dragon.springboot.web;
+
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DragonWebApplication.class);
+	}
+
+}
